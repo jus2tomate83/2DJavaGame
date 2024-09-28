59ce61d75278698d4f0234f235e8f39655d2cd0d
jus2tomate83 <blefebvre595@gmail.com>
2024-09-28 12:46:15 +0200

    1

---
+++ src/main/GamePanel.java
@@ -0,0 +1,29 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+
+    //SCREEN SETTINGS
+    final int originalTilesSize = 16;  //16x16 tiles
+    final int scale = 3;
+
+    final int tilesSize = originalTilesSize * scale; // 48x48 tiles
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tilesSize * maxScreenCol; //768 pixels
+    final int screenHeight= tilesSize * maxScreenRow; //576 pixels
+
+
+    public GamePanel(){
+
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.BLACK);
+        this.setDoubleBuffered(true);
+    }
+
+
+
+
+}
