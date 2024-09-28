a6481ca636e1ef1c6b3240756ffaee5b5f3afb56
jus2tomate83 <blefebvre595@gmail.com>
2024-09-28 12:49:22 +0200

    Satup Window

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,7 +10,10 @@ public class Main {
         window.setResizable(false);
         window.setTitle("2D adventure");
 
-        GamePanel gamePanel
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
