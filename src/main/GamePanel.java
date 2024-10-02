61a9d9a2b17be90e10ffba704380e4d383538954
jus2tomate83 <blefebvre595@gmail.com>
2024-10-02 17:53:17 +0200

    main boucle entity , Player class

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,30 +1,95 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
 
     //SCREEN SETTINGS
     final int originalTilesSize = 16;  //16x16 tiles
     final int scale = 3;
 
-    final int tilesSize = originalTilesSize * scale; // 48x48 tiles
+    public final int tilesSize = originalTilesSize * scale; // 48x48 tiles
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tilesSize * maxScreenCol; //768 pixels
     final int screenHeight= tilesSize * maxScreenRow; //576 pixels
 
+
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this,keyH);
+
+
+    //set player default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
+
+    //FPS
+    int FPS = 60;
 
     public GamePanel(){
-
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.BLACK);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
+    }
+
+    public void startGameThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
     }
 
 
+    @Override
+    public void run() {
+
+        double drawInterval = 1000000000/FPS; //0.01666 sec
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while (gameThread != null){
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1){
+                // 1: Update information sutch as character position
+                Update();
+                //2; draw the screen with the update information
+                repaint();
+                delta --;
+                drawCount++;
+
+            }
+            if (timer>=1000000000){
+                System.out.println("FPS : " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
 
 
+        }
+
+    }
+    public void Update(){
+        player.update();
+    }
+
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+        player.draw(g2);
+        g2.dispose();
+    }
 }
